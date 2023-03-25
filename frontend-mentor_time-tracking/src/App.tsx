3699fec966e415f3d95d5b03ff4dbcc22f0fba0d
Maggus <mail4markus.schneider@gmx.de>
2023-03-25 16:54:17 +0100

    refactor: center main container

--- frontend-mentor_time-tracking/src/App.tsx
+++ frontend-mentor_time-tracking/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <header></header>
       <nav></nav>
-      <main className=" bg-very-dark-blue grid w-full h-full ">
+      <main className=" bg-very-dark-blue w-full h-full ">
         <TimeTracker />
       </main>
       <footer></footer>
